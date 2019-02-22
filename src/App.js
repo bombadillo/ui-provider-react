2a169ed37134a9f80677e2b75a006ea46f134def
bombadillo <mumfordnsons@gmail.com>
2019-02-22 14:58:16 +0000

    Initial commit :tada:

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Latest deals</h2>
+        <ul>
+          <li>Taco Deal 1</li>
+          <li>Taco Deal 2</li>
+          <li>Taco Deal 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactWebComponent from 'react-web-component';
+import './index.css';
+import App from './App';
+
+ReactWebComponent.create(<App />, 'my-component');
