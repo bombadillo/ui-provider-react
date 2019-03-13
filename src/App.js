2742da55bafc200c05f20ecb71b68e59ee0c27e6
bombadillo <mumfordnsons@gmail.com>
2019-03-13 16:42:51 +0000

    Make work with styled components!!!!!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Grid } from "chassis-react";
+import styled, { StyleSheetManager } from "styled-components";
+
+import "./App.css";
+
+let Header = styled.h2`
+  font-size: 48px;
+`;
 
 class App extends Component {
+  mainEl = undefined;
+
+  constructor() {
+    super();
+
+    this.state = {
+      shadowRoot: undefined
+    };
+
+    this.mainEl = React.createRef();
+  }
+
+  componentDidMount() {
+    this.hasShadowParent(this.mainEl.current);
+  }
+
+  hasShadowParent = element => {
+    console.log(element);
+
+    if (!element) {
+      return;
+    }
+
+    while (element.parentNode && (element = element.parentNode)) {
+      console.log(element);
+      if (element instanceof ShadowRoot) {
+        console.log("has shadow root");
+        this.setState({ shadowRoot: element });
+        return true;
+      }
+    }
+    console.log("has no shadow root");
+    return false;
+  };
+
   render() {
     return (
-      <div className="App">
-        <h2>Latest deals</h2>
-        <ul>
-          <li>Taco Deal 1</li>
-          <li>Taco Deal 2</li>
-          <li>Taco Deal 3</li>
-        </ul>
-      </div>
+      <Grid container>
+        <div className="App" ref={this.mainEl}>
+          {this.state.shadowRoot && (
+            <StyleSheetManager target={this.state.shadowRoot}>
+              <Header>Latest deals</Header>
+            </StyleSheetManager>
+          )}
+
+          <ul>
+            <li>Taco Deal 1</li>
+            <li>Taco Deal 2</li>
+            <li>Taco Deal 3</li>
+          </ul>
+        </div>
+      </Grid>
     );
   }
 }
